98b5228efbb57dd3d8772378aed94fbfa97a983b
iCodeSpree <icodespree@gmail.com>
2021-07-31 16:12:17 -0500

    Initial Commit

---
+++ Safal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safal
+//
+//  Created by Shreya Jhala on 7/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
